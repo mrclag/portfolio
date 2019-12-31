64256744752f4f7cdecbf6474ee7051da570acb5
mrclag <mattrclagett@gmail.com>
2019-12-30 17:31:07 -0800

    Updates

---
+++ precache-manifest.6c97044ab4c501f0799bc5537539b53a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d154dd5ef487a07a0c264420d7783e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95472afe4024f5f48eba",
+    "url": "/static/css/main.76cceaea.chunk.css"
+  },
+  {
+    "revision": "e8f60c787cb7fb19afdc",
+    "url": "/static/js/2.bcb86856.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.bcb86856.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95472afe4024f5f48eba",
+    "url": "/static/js/main.78e7f25c.chunk.js"
+  },
+  {
+    "revision": "5b038f259db629f91f35",
+    "url": "/static/js/runtime-main.d6051940.js"
+  },
+  {
+    "revision": "e9c9c9e81a1107f0e0301a14d286cd49",
+    "url": "/static/media/EmojiTetris.e9c9c9e8.png"
+  },
+  {
+    "revision": "56ccea2807b4b62c3c91058356112746",
+    "url": "/static/media/PatternOverlay.56ccea28.png"
+  },
+  {
+    "revision": "c81818e5de2be7c8788672749e0d8a74",
+    "url": "/static/media/ReleVocab.c81818e5.png"
+  },
+  {
+    "revision": "3d228c0c68a96db3f5dc4a533c9e8e36",
+    "url": "/static/media/icecream.3d228c0c.png"
+  },
+  {
+    "revision": "8e18e1d6862bca794510cb762c432a73",
+    "url": "/static/media/resume_matthew_clagett.8e18e1d6.pdf"
+  }
+]);
